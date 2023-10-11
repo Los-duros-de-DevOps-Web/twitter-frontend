3e3bf54fc46f68a8a3733471d682aa5d628d148a
GabrielSB19 <gasuarez2002@gmail.com>
2023-10-11 07:40:18 -0500

    Added the auth

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,16 +1,21 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 
-import fetcher from '@/libs/fetcher';
+import fetcher from "@/libs/fetcher";
 
 const useCurrentUser = () => {
-  const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
+  const { data, error, isLoading, mutate } = useSWR(
+    "http://localhost:3002/current",
+    fetcher
+  );
+
+  console.log("data", data);
 
   return {
     data,
     error,
     isLoading,
-    mutate
-  }
+    mutate,
+  };
 };
 
 export default useCurrentUser;
