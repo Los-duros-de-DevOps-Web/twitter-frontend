9f5352fcc72ac4e5fd0af8b035eb19f02e451454
GabrielSB19 <gasuarez2002@gmail.com>
2023-10-11 08:31:56 -0500

    Starting the transition with the util service

--- hooks/useLike.ts
+++ hooks/useLike.ts
@@ -7,7 +7,7 @@ import useLoginModal from "./useLoginModal";
 import usePost from "./usePost";
 import usePosts from "./usePosts";
 
-const useLike = ({ postId, userId }: { postId: string, userId?: string }) => {
+const useLike = ({ postId, userId }: { postId: string; userId?: string }) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedPost, mutate: mutateFetchedPost } = usePost(postId);
   const { mutate: mutateFetchedPosts } = usePosts(userId);
@@ -29,25 +29,33 @@ const useLike = ({ postId, userId }: { postId: string, userId?: string }) => {
       let request;
 
       if (hasLiked) {
-        request = () => axios.delete('/api/like', { data: { postId } });
+        request = () =>
+          axios.delete("http://localhost:3003/like", { data: { postId } });
       } else {
-        request = () => axios.post('/api/like', { postId });
+        request = () => axios.post("/http://localhost:3003/like", { postId });
       }
 
       await request();
       mutateFetchedPost();
       mutateFetchedPosts();
 
-      toast.success('Success');
+      toast.success("Success");
     } catch (error) {
-      toast.error('Something went wrong');
+      toast.error("Something went wrong");
     }
-  }, [currentUser, hasLiked, postId, mutateFetchedPosts, mutateFetchedPost, loginModal]);
+  }, [
+    currentUser,
+    hasLiked,
+    postId,
+    mutateFetchedPosts,
+    mutateFetchedPost,
+    loginModal,
+  ]);
 
   return {
     hasLiked,
     toggleLike,
-  }
-}
+  };
+};
 
 export default useLike;
