24218957ed796866fd2f336298a8d1ed57e3644c
Johan Stiven Ricardo <69874418+Johan794@users.noreply.github.com>
2023-11-13 21:27:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- env: {
-    URL_UTIL: process.env.URL_UTIL || "default-value-if-secret-not-available",
-    URL_USER: process.env.URL_USER || "default-value-if-secret-not-available",
-    URL_AUTH: process.env.URL_AUTH || "default-value-if-secret-not-available",
-    URL_FRONT: process.env.URL_FRONT || "default-value-if-secret-not-available",
-    URL_POSTNOTI: process.env.URL_POSTNOTI || "default-value-if-secret-not-available",
-  },
 };
 
 module.exports = nextConfig;
